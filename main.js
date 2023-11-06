59cb41525319e253a33c0e2e1d9393694a0cbb5f
Tóth Kevin <tothkevin0716@gmail.com>
2023-11-06 15:09:33 +0100

    Html form, initialize variables

--- main.js
+++ main.js
@@ -1,2 +1,6 @@
 import quotes from './quotes.json';
 
+const buttonAll = document.getElementById("buttonAll");
+const buttonThe = document.getElementById("buttonThe");
+const buttonLength = document.getElementById("buttonLength");
+const inputAmount = document.getElementById("inputAmount");
